4dd3f2341dcd24cd1772e0e4e90331384df62257
Rami <ramibilal@live.com>
2020-08-25 15:02:17 -0500

    Added download CSV functionality

--- grab_tou_data.py
+++ grab_tou_data.py
@@ -14,8 +14,13 @@ class GrabTOUData:
         prefs = {'download.default_directory': ".\\sample-data\\TOU_figures"}
         self.options.add_experimental_option('prefs', prefs)
         self.options.headless = True
+        self.driver = None
 
     def grab_today_tomorrow_data(self):
+        """
+        Uses selenium to load driver and webpage containing the TOU tables
+        :return: Returns a tuple of todays and tomorrows TOU data in a pandas dataframe
+        """
         self.driver = webdriver.Chrome('chromedriver.exe', options=self.options)
         todays_table_url = "https://energy-db.energy-stats.uk/d-solo/Y9Oh1wmZk/octopus-agile-tariff-dashboard?org" \
                            "Id=1&from=now%2Fd&to=now%2Fd&var-area_name=London&panelId=6"
@@ -27,6 +32,11 @@ class GrabTOUData:
         return todays_prices_df, tomorrows_prices_df
 
     def _parse_to_dataframe(self, url: str) -> pd.DataFrame:
+        """
+        Parses TOU table from webpage and converts to dataframe
+        :param url: url of table to be parsed
+        :return: returns single dataframe from parsed table from webpage
+        """
         self.driver.get(url)
         xpath = '/html/body/grafana-app/div/div/div/react-container/div/div/div/div/' \
                 'plugin-component/panel-plugin-table/grafana-panel/div/div[2]/ng-transclude' \
@@ -51,10 +61,27 @@ class GrabTOUData:
 
         return prices_df
 
-    def download_today_tomorrow_csv(self):
+    @staticmethod
+    def download_tou_csv():
+        """
+        Downloads CSV file containing all-time TOU data, dumps data to CSV, parses out todays and tomorrows data and
+        converts to respective csv files
+        :return: None
+        """
         response = requests.get('https://www.energy-stats.uk/wp-content/historic-data/csv_agileoutgoing_C_London.csv')
-        with open('./TOU_Data/full_data.csv', 'w') as file:
-            writer = csv.writer(file)
-            for line in response.iter_lines():
-                writer.writerow(line.decode('utf-8').split(','))
+        with open('./TOU_Data/full_data.csv', 'wb') as file:
+            file.write(response.content)
 
+        decoded_content = response.content.decode('utf-8')
+        rows = list(csv.reader(decoded_content.splitlines()))
+
+        tomorrows_rows = rows[-46:]
+        todays_rows = rows[-94:-45]
+
+        with open('./TOU_Data/tomorrows_data.csv', 'w', newline="") as tomorrow_csv:
+            writer = csv.writer(tomorrow_csv)
+            writer.writerows(tomorrows_rows)
+
+        with open('./TOU_Data/todays_data.csv', 'w', newline="") as today_csv:
+            writer = csv.writer(today_csv)
+            writer.writerows(todays_rows)
