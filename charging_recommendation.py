3ea4a4515330209480e736c08e8d3ae7238eed08
AtomAnu <a.anuchitanukul@gmail.com>
2020-08-27 22:02:25 +0100

    Update charging_recommendation.py

--- charging_recommendation.py
+++ charging_recommendation.py
@@ -31,34 +31,41 @@ class charging_recommendation(object):
         pred['charging'] = 0
         pred['journey'] = 0
 
+        # iterate through all pairs of journey start and end points
         for start, end in zip(self.journey_start, self.journey_end):
 
+            # find correct time slots for start and end points
             start_time_slot = start.floor(freq='30min')
             end_time_slot = end.floor(freq='30min')
 
+            # fully fill any time slots between start_time_slot and end_time_slot
             pred.loc[np.logical_and(pred.index > start_time_slot, pred.index < end_time_slot), 'charging'] = 30
 
+            # fill start_time_slot and end_time_slot based on journey time
             if start_time_slot == end_time_slot:
                 pred.loc[start_time_slot, ['charging', 'journey']] += (end - start).seconds / 60
             else:
                 pred.loc[start_time_slot, ['charging', 'journey']] += (start.ceil(freq='30min') - start).seconds / 60
                 pred.loc[end_time_slot, ['charging', 'journey']] += (end - end_time_slot).seconds / 60
 
+            # calculate total charging time for the journey
             journey_energy_consumption = sum(self.EV_data.loc[start:end]['P_total'])
             charge_time = journey_energy_consumption / (charger_power * 60)
 
-            #number of time slots needed to charge EV
-            quotient = int(charge_time // 30) 
-            remainder = charge_time % 30 
+            # calculate number of time slots needed to charge EV
+            quotient = int(charge_time // 30) # full slots
+            remainder = charge_time % 30
 
-            # --------- and sorting TOU by price
+            # ignore any full slots and sort TOU slots by price
             free_time_slots = pred.loc[np.logical_and(pred.index < start, pred['charging'] < 30)].copy()
             free_time_slots = free_time_slots.sort_values(by=['TOU'])
 
+            # exception handling
             if charge_time + sum(free_time_slots['charging']) > 30 * len(free_time_slots):
                 print('Not enough time slots to charge')
                 return None
 
+            # fill in slots based on the remainder with the quotient as offset
             remainder += sum(free_time_slots.iloc[list(range(0, quotient))]['charging'])
             idx_offset = 0
             while remainder != 0:
@@ -72,8 +79,10 @@ class charging_recommendation(object):
 
                 idx_offset += 1
 
+            # fully fill the very first cheapest slots
             if quotient > 0: pred.loc[free_time_slots.iloc[list(range(0, quotient))].index, 'charging'] = 30
 
+        # subtract journey time from charging time
         pred['charging'] -= pred['journey']
         self.EV_data['charging'] = pred['charging']
 
