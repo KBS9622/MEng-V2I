b3bd96860890c97bc23b2abfe6e67ee88a66e7ef
Rami <ramibilal@live.com>
2020-11-22 14:34:13 +0300

    Added Simulation fie : a replacement for main file that is more modular, reusable and configurable for simulation purposes

--- src/playground.py
+++ src/playground.py
@@ -1,26 +1,18 @@
-import os
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 import pandas as pd
+import requests
+import csv
+
+driver = webdriver.Chrome('../utils/chromedriver.exe')
 
 
-def load_csv_data(file_name, subdir=''):
-    """
-    Loads data from .csv file in to DataFrame
-
-    :param file_name: .csv file name in string
-    :param subdir: optional parameter to specify the subdirectory of the file
-    :return: extracted data in DataFrame
-    """
-
-    file_dir = os.path.realpath('../')
-    for root, dirs, files in os.walk(file_dir):
-        if root.endswith(subdir):
-            for name in files:
-                if name == file_name:
-                    file_path = os.path.join(root, name)
-
-    df = pd.read_csv(file_path)
-
-    return df
 
 
-test = load_csv_data('2012-03-01.csv', '1119055_1')
+
+
+
+
--- src/simulation.py
+++ src/simulation.py
@@ -4,40 +4,79 @@ from src.TOU_analysis_and_prediction import TOU
 from src.charging_recommendation import charging_recommendation
 from pandas.tseries.offsets import DateOffset
 
-class Simulation:
-    def __init__(self, drive_cycle_file, drive_cycle_subdir, tou_file):
-        tou_model_trained = False
-        if not tou_model_trained:
-            self.train_tou_model()
 
-        self.ev_cycle_file = drive_cycle_file
-        self.ev_subdirectory = drive_cycle_subdir
+class Simulation:
+    def __init__(self, drive_cycle_file, drive_cycle_subdir, tou_file, train_tou):
+        self.min_tou_threshold = 0
+        self.tou_obj = TOU(tou_file)
+        if train_tou:
+            self.tou_obj.create_and_fit_model()
+        self.ev_obj = EV(drive_cycle_file, drive_cycle_subdir)
+        self.graph_plotter(drive_cycle_file, drive_cycle_subdir)
+
+    def plugged_in(self):
+        """
+        
+        :return: 
+        """
+        # self.start_time = time.
+        # self.run_recommendation_algorithm()
+        pass
 
     def run_recommendation_algorithm(self):
-        ev = self.create_ev()
-        tou = create_tou()
-        recommendation_results = charging_recommendation()
+        """
+        
+        :return: 
+        """
+        predicted_tou_data = self.get_tou_data()
+        ev_consumption_data = self.format_ev_data()
+        recommendation_results = charging_recommendation(ev_consumption_data, predicted_tou_data)
+        return recommendation_results
 
-    def create_ev(self):
-        ev_obj = EV(self.ev_cycle_file, self.ev_subdirectory)
+    def format_ev_data(self):
+        """
+        
+        :return: 
+        """
         cols_to_drop = ['cycle_sec', 'timestep', 'speed_mph', 'accel_meters_ps', 'speed_mps',
                         'accel_mps2', 'P_wheels', 'P_electric_motor', 'n_rb', 'P_regen']
 
-        P_total = ev_obj.data.copy()
-        P_total = P_total.drop(columns=cols_to_drop)
-        P_total = P_total.set_index('timestamp')
+        p_total = self.ev_obj.data.copy()
+        p_total = p_total.drop(columns=cols_to_drop)
+        p_total = p_total.set_index('timestamp')
+        p_total = p_total.set_index(p_total.index
+                                    + DateOffset(days=(self.start_time.floor(freq='D')
+                                                       - p_total.iloc[0].name.floor(freq='D')).days))
+        return p_total
 
-    def create_tou(self):
-        pass
+    def get_tou_data(self, start_time="", end_time="" ):
+        """
+        
+        :param start_time: 
+        :param end_time: 
+        :return: 
+        """
+        self.start_time = pd.to_datetime('2019-01-31 00:00:00')
+        self.end_time = pd.to_datetime('2019-01-31 23:30:00')
+        predicted_tou = self.tou_obj.predict_and_compare(start_time, end_time)
+        return predicted_tou
 
-    def train_tou_model(self):
-        pass
+    def graph_plotter(self, file, subdir):
+        """
+        
+        :return: 
+        """
+        y = ['P_electric_motor', 'speed_mps', 'P_regen', 'n_rb', 'soc', 'P_total']
+        file_name = ['energy_consumption.png', 'speed_profile.png', 'energy_consumption_with_regen.png',
+                     'n_rb.png', 'soc.png', 'total_energy_conumption.png']
+        self.ev_obj.graph_plotter(y=y, file_name=file_name, subdir=subdir, date=file.strip('.csv'))
 
-
-
-    def run_recommendation_algorithm(self):
-        pass
-
-    def graph_plotter(self):
+    def result_plotter(self):
+        """
+        
+        :return: 
+        """
+        without_recommendation = ''
+        without_recommendation = ''
         pass
 
