d96d154cd858c6fc7ac4f2d64c3387d42d236073
Rami <ramibilal@live.com>
2020-11-23 16:57:07 +0300

    merging from SOC_consideration branch

--- src/EV_data_analysis.py
+++ src/EV_data_analysis.py
@@ -3,31 +3,31 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class EV(object):
-    
     excess = None
     deficit = None
 
-    g = 9.8066 # gravity (m/s)
-    theta = 0 # road grade
+    g = 9.8066  # gravity (m/s)
+    theta = 0  # road grade
 
     EV_model = None
-    m_kg = None # mass (kg)
+    m_kg = None  # mass (kg)
 
-    #rolling resistance parameters
+    # rolling resistance parameters
     C_r = None
     c_1 = None
     c_2 = None
 
-    rho_air = None # air mass density (kg/m3)
-    A_f = None # frontal area of the vehicle (m2)
-    C_D = None # aerodynamic drag coefficient of the vehicle
-    n_driveline = None # driveline efficiency
-    n_electric_motor = None # electric motor efficiency (85%-95% for Nissan Leaf)
+    rho_air = None  # air mass density (kg/m3)
+    A_f = None  # frontal area of the vehicle (m2)
+    C_D = None  # aerodynamic drag coefficient of the vehicle
+    n_driveline = None  # driveline efficiency
+    n_electric_motor = None  # electric motor efficiency (85%-95% for Nissan Leaf)
 
-    capacity = None #In Wh
-    charge_lvl = None #In Wh
-    soc = None #State of charge of the battery in %
+    capacity = None  # In Wh
+    charge_lvl = None  # In Wh
+    soc = None  # State of charge of the battery in %
 
     data = None
 
@@ -35,7 +35,7 @@ class EV(object):
 
         self.file_name = file_name
         self.subdir = subdir
-        self.c_file_name = c_file_name # EV characteristics file name
+        self.c_file_name = c_file_name  # EV characteristics file name
         self.choice = choice
         self.data = self.format_EV_data()
 
@@ -52,27 +52,24 @@ class EV(object):
         self.n_driveline = self.EV['n_driveline']
         self.n_electric_motor = self.EV['n_electric_motor']
         self.capacity = self.EV['capacity']
-        self.charge_lvl = self.capacity * (50/100) #battery is 50% charged initially
+        self.charge_lvl = self.capacity * (50 / 100)  # battery is 50% charged initially
 
         self.calculate_energy_consumption()
 
-    # Put as helper functions, used globally
     def load_csv_data(self, file_name, subdir=''):
         """
         Loads data from .csv file in to DataFrame
-
         :param file_name: .csv file name in string
         :param subdir: optional parameter to specify the subdirectory of the file
         :return: extracted data in DataFrame
         """
 
-        file_dir = os.path.realpath('../')
+        file_dir = os.path.realpath('./')
         for root, dirs, files in os.walk(file_dir):
             if root.endswith(subdir):
                 for name in files:
                     if name == file_name:
                         file_path = os.path.join(root, name)
-                        break
 
         df = pd.read_csv(file_path)
 
@@ -81,7 +78,6 @@ class EV(object):
     def format_EV_data(self):
         """
         Formats date and time in the 'timestamp' column
-
         :return: formatted DataFrame
         """
 
@@ -94,7 +90,6 @@ class EV(object):
     def EV_menu(self):
         """
         Menu for selection of EV to be used in system
-
         :return: dataframe containing parameters for selected EV
         """
 
@@ -119,7 +114,6 @@ class EV(object):
     def calculate_energy_consumption(self):
         """
         Calculates the energy consumption trend and plots it against time
-
         :return: data in DataFrame with 4 new columns: speed in m/s, acceleration in m/s^2,
                                                         power at wheels in W and power at electric motor in W
         """
@@ -129,10 +123,11 @@ class EV(object):
         self.data['accel_mps2'] = mph_to_mps * self.data['accel_meters_ps']
 
         # Power required at the wheels
-        self.data['P_wheels'] = (self.m * self.data['accel_mps2'] \
-                        + self.m * self.g * np.cos(self.theta) * self.C_r * 1e-3 * (self.c_1 * self.data['speed_mps'] + self.c_2) \
-                        + 0.5 * self.rho_air * self.A_f * self.C_D * (self.data['speed_mps']**2) \
-                        + self.m * self.g * np.sin(self.theta)) * self.data['speed_mps']
+        self.data['P_wheels'] = (self.m * self.data['accel_mps2'] + self.m * self.g * np.cos(self.theta) *
+                                 self.C_r * 1e-3 * (
+                                             self.c_1 * self.data['speed_mps'] + self.c_2)
+                                 + 0.5 * self.rho_air * self.A_f * self.C_D * (self.data['speed_mps'] ** 2)
+                                 + self.m * self.g * np.sin(self.theta)) * self.data['speed_mps']
 
         # Power at electric motor
         self.data['P_electric_motor'] = self.data['P_wheels'] / (self.n_driveline * self.n_electric_motor)
@@ -145,7 +140,6 @@ class EV(object):
         """
         Calculates the energy consumption (with regenerative braking efficiency, auxiliary loads and model error included)
         trend and plots it against time
-
         :param data: data in DataFrame
         :return: data in DataFrame with 3 new columns: regenerative braking efficiency,
                                                         power at electric motor adjusted with n_rb
@@ -154,8 +148,8 @@ class EV(object):
         alpha = 0.0411
 
         # regenerative braking efficiency is ZERO when acceleration >= 0
-        self.data['n_rb'] = (np.exp(alpha / abs(self.data['accel_mps2']) ) )**-1
-        self.data['n_rb'].where(self.data['accel_mps2'] < 0, other = 0, inplace = True)
+        self.data['n_rb'] = (np.exp(alpha / abs(self.data['accel_mps2']))) ** -1
+        self.data['n_rb'].where(self.data['accel_mps2'] < 0, other=0, inplace=True)
 
         # calculate the energy being stored back to the battery whenever the car decelerates
         self.data['P_regen'] = self.data['P_electric_motor']
@@ -163,57 +157,55 @@ class EV(object):
 
         # add the energy consumption when the car accelerates
         pos_energy_consumption = self.data['P_electric_motor'].copy()
-        pos_energy_consumption.where(self.data['accel_mps2']>=0, other=0, inplace = True)
+        pos_energy_consumption.where(self.data['accel_mps2'] >= 0, other=0, inplace=True)
         self.data['P_regen'] += pos_energy_consumption
 
         # add the energy consumption of auxiliary loads and model error
-        auxiliary = 700 # Watts or Joules per second
-        average_model_error = -5.9 #percent
-        self.data['P_total'] = (self.data['P_regen'] + auxiliary)*(100 + average_model_error)/100
+        auxiliary = 700  # Watts or Joules per second
+        average_model_error = -5.9  # percent
+        self.data['P_total'] = (self.data['P_regen'] + auxiliary) * (100 + average_model_error) / 100
 
     def charge(self, power_in_joules):
         """
         Method to charge EV battery, accounting for battery efficiency
-
         :param data: power_in_joules [power (because data is measured every second)]
         :return: new SOC for the EV object
         """
 
-        #maximum SOC to ensure safe operation
-        max_soc = 95 # in %
-        max_charge_lvl = (max_soc/100) * self.capacity
+        # maximum SOC to ensure safe operation
+        max_soc = 95  # in %
+        max_charge_lvl = (max_soc / 100) * self.capacity
 
-        n_battery = 90 #battery efficiency
+        n_battery = 90  # battery efficiency
         Wh_to_J = 3600
-        power = (power_in_joules / Wh_to_J) / (n_battery / 100) #convert joules to Wh
+        power = (power_in_joules / Wh_to_J) / (n_battery / 100)  # convert joules to Wh
 
         if (max_charge_lvl - self.charge_lvl) >= power:
             self.charge_lvl += power
         else:
-            temp = self.charge_lvl+power-max_charge_lvl
+            temp = self.charge_lvl + power - max_charge_lvl
             self.excess += temp
             print('Battery is full, {} Wh of excess energy'.format(temp))
             self.charge_lvl = max_charge_lvl
 
         # calculates the new instantaneous SOC
-        self.soc = (self.charge_lvl/self.capacity) * 100
+        self.soc = (self.charge_lvl / self.capacity) * 100
 
-    def discharge(self,power_in_joules): 
+    def discharge(self, power_in_joules):
         """
         Method to discharge EV battery, accounting for battery efficiency
-
         :param data: power (because data is measured every second)
         :return: new SOC for the EV object
         """
 
-        #minimum SOC to ensure safe operation
+        # minimum SOC to ensure safe operation
         min_soc = 20
-        min_charge_lvl = (min_soc/100) * self.charge_lvl
+        min_charge_lvl = (min_soc / 100) * self.charge_lvl
 
-        n_battery = 90 #battery efficiency
+        n_battery = 90  # battery efficiency
         Wh_to_J = 3600
-        power = (power_in_joules / Wh_to_J) / (n_battery / 100) #convert joules to Wh
-        
+        power = (power_in_joules / Wh_to_J) / (n_battery / 100)  # convert joules to Wh
+
         if power > (self.charge_lvl - min_charge_lvl):
             temp = power - (self.charge_lvl - min_charge_lvl)
             self.deficit += temp
@@ -223,12 +215,11 @@ class EV(object):
             self.charge_lvl -= power
 
         # calculates the new instantaneous SOC
-        self.soc = (self.charge_lvl/self.capacity) * 100
+        self.soc = (self.charge_lvl / self.capacity) * 100
 
     def soc_over_time(self):
         """
         Calculates the SOC and charge level of the vehicle over time
-
         :param data: -
         :return: data in DataFrame with 2 new columns: SOC of EV over time
                                                         and charge level of EV over time
@@ -243,21 +234,20 @@ class EV(object):
         self.data['charge_lvl'] = timeseries_charge_lvl
 
     def graph_plotter(self, x='timestamp', y='P_regen', file_name='energy_consumption_with_regen.png',
-                    subdir='test', date='test'):
+                      subdir='test', date='test'):
         """
         Plots a graph according to the specified x and y, and saves it to the specified file name
-
         :param data: data in DataFrame
         :param x: the name of the column for the x axis
         :param y: the name(s) of the column for the y axis
         :param file_name: the file name(s) to store the plot(s)
         """
-        figure_folder = 'data/EV_figures'
-        directory = figure_folder+'/'+subdir+'/'+self.EV_model+'/'+date # directory to store the figures
+        figure_folder = 'EV_figures'
+        directory = figure_folder + '/' + subdir + '/' + self.EV_model + '/' + date  # directory to store the figures
 
         if not os.path.exists(directory):
             os.makedirs(directory)
 
         for col, name in zip(y, file_name):
             self.data.plot(x=x, y=col)
-            plt.savefig(directory+'/'+name)
+            plt.savefig(directory + '/' + name)
--- src/charging_recommendation.py
+++ src/charging_recommendation.py
@@ -1,16 +1,54 @@
 import pandas as pd
 import numpy as np
+import json
+from datetime import datetime, timedelta
 
 
 class charging_recommendation(object):
 
-    def __init__(self, EV_data, TOU_data):
-
+    def __init__(self, EV_data, TOU_data, previous_EV_data=None):
         self.EV_data = EV_data
         self.TOU_data = TOU_data
-        self.journey_start, self.journey_end = self.find_journey_start_and_end_points()
+        self.journey_start, self.journey_end = self.find_journey_start_and_end_points(data=self.EV_data)
+        self.previous_end = None
+        # if system inputs the starting scenario, then create these variables
+        if not previous_EV_data:
+            self.previous_end = [previous_EV_data.iloc[-1, :].name]
 
-    def find_journey_start_and_end_points(self, min_gap=30):
+    def set_EV_data(self, new_EV_data):
+        """
+        Method to update the variable self.EV_Data and use previous data to determine reference point for charging availability
+
+        :param data: new_EV_data (the predicted drive cycle)
+        :return: -
+        """
+        start_time_new_EV_data = new_EV_data.iloc[0, :].name
+        end_time_new_EV_data = new_EV_data.iloc[-1, :].name
+        if start_time_new_EV_data > self.EV_data.iloc[-1, :].name:
+            # if the EV plug in time is past the end of the last predicted drive cycle
+            self.previous_end = self.journey_end[-1,
+                                :]  # get the last predicted journey end time as a reference to start of charging availability
+            if self.journey_end[-1, :] < end_time_new_EV_data - timedelta(
+                    hours=self.config_dict['System_consideration_span']):
+                self.previous_end = end_time_new_EV_data - timedelta(
+                    hours=self.config_dict['System_consideration_span'])
+        else:
+            # if the EV plug in time is before the end of the last predicted drive cycle
+            self.previous_end = [self.EV_data.loc[start_time_new_EV_data,
+                                 :].name]  # plug in time (start_time_new_EV_data) is the starting reference for charging availability
+        self.EV_data = new_EV_data
+        self.journey_start, self.journey_end = self.find_journey_start_and_end_points(data=self.EV_data)
+
+    def set_TOU_data(self, new_TOU_data):
+        self.TOU_data = new_TOU_data
+
+    def update_user_config(self, config_path):
+        # open the json file and load the object into a python dictionary
+        with open(config_path) as f:
+            self.config_dict = json.load(f)
+        # maybe have a seperate file to initialise config parameters based on user input, like calculating emergency reserves based on location
+
+    def find_journey_start_and_end_points(self, data, min_gap=30):
         """
         Method to indetify sub-journey start/end times (Classifier Module)
 
@@ -18,8 +56,8 @@ class charging_recommendation(object):
         :return: journey_start varible (with sub-journey start times) and journey_end variable (with corresponding sub-journey end times)
         """
 
-        P_total_data = self.EV_data.copy()
-        min_gap = pd.Timedelta(min_gap*60, unit='sec')
+        P_total_data = data.copy()
+        min_gap = pd.Timedelta(min_gap * 60, unit='sec')
 
         journey_start = [P_total_data.iloc[0, :].name]
         journey_end = [P_total_data.iloc[-1, :].name]
@@ -28,45 +66,92 @@ class charging_recommendation(object):
             time_diff = P_total_data.iloc[i + 1, :].name - P_total_data.iloc[i, :].name
             if time_diff >= min_gap:
                 journey_start.append(P_total_data.iloc[i + 1, :].name)
-                journey_end.insert(len(journey_end)-1, P_total_data.iloc[i, :].name)
+                journey_end.insert(len(journey_end) - 1, P_total_data.iloc[i, :].name)
 
         return journey_start, journey_end
 
-    def recommend(self, threshold=0, charger_power=3e3):
+    def recommend(self):
         """
         Method to recommend charging times (Scheduler Module)
 
-        :param data: threshold (user-defined/system defined TOU threshold) and charger_power
+        :param data: JSON containing configuration parameters
         :return: recommmended charging slots
         """
+        # **** maybe before the system runs the scheduler, it should use API to request up to date info from EV, like SOC and charge level
+        # could add limit to when the EV is actually home and ready to charge (by reading the current passing through charger when initially plugged in)
+        Wh_to_J = 3600
+        initial_charge = self.config_dict['Charge_level'] * Wh_to_J
+        battery_capacity = self.config_dict['EV_info']['Capacity'] * Wh_to_J
+
+        lower_limit = battery_capacity * (self.config_dict['Lower_buffer'] + self.config_dict[
+            'Emergency_reserves']) / 100  # lower limit can also include the buffer the manufacturers set *if the charge level obtained from EV has not considered that yet
+        upper_limit = battery_capacity * (
+                    self.config_dict['Upper_buffer'] / 100)  # does not consider the manufacturer upper buffer yet
+        available_charge = initial_charge - lower_limit
+        expected_charge = initial_charge
 
         pred = self.TOU_data.copy()
         pred['charging'] = 0
         pred['journey'] = 0
 
+        # TEMPORARY CODE: to determine available charging slots (assumption: not available to charge from start of 1st journey to end of last journey)
+        start_of_day = self.journey_start[0]
+        print(start_of_day)
+        end_of_day = self.journey_end[-1]
+        print(end_of_day)
+
+        # find correct time slots for start and end points
+        start_time_slot = start_of_day.floor(freq='30min')
+        end_time_slot = end_of_day.floor(freq='30min')
+
+        # fully fill any time slots between start_time_slot and end_time_slot
+        pred.loc[np.logical_and(pred.index > start_time_slot, pred.index < end_time_slot), ['charging', 'journey']] = 30
+
+        # fill start_time_slot and end_time_slot based on journey time
+        if start_time_slot == end_time_slot:
+            pred.loc[start_time_slot, ['charging', 'journey']] += (end_of_day - start_of_day).seconds / 60
+        else:
+            pred.loc[start_time_slot, ['charging', 'journey']] += (start_of_day.ceil(
+                freq='30min') - start_of_day).seconds / 60
+            pred.loc[end_time_slot, ['charging', 'journey']] += (end_of_day - end_time_slot).seconds / 60
+        print(pred)
+
         # iterate through all pairs of journey start and end points
         for start, end in zip(self.journey_start, self.journey_end):
 
-            # find correct time slots for start and end points
-            start_time_slot = start.floor(freq='30min')
-            end_time_slot = end.floor(freq='30min')
+            # TEMPORARY COMMENTED: assumption is that we can only charge after end of last journey and before first journey of tmr, so the code below is redundant
+            # # find correct time slots for start and end points
+            # start_time_slot = start.floor(freq='30min')
+            # end_time_slot = end.floor(freq='30min')
 
-            # fully fill any time slots between start_time_slot and end_time_slot
-            pred.loc[np.logical_and(pred.index > start_time_slot, pred.index < end_time_slot), ['charging', 'journey']] = 30
+            # # fully fill any time slots between start_time_slot and end_time_slot
+            # pred.loc[np.logical_and(pred.index > start_time_slot, pred.index < end_time_slot), ['charging', 'journey']] = 30
 
-            # fill start_time_slot and end_time_slot based on journey time
-            if start_time_slot == end_time_slot:
-                pred.loc[start_time_slot, ['charging', 'journey']] += (end - start).seconds / 60
+            # # fill start_time_slot and end_time_slot based on journey time
+            # if start_time_slot == end_time_slot:
+            #     pred.loc[start_time_slot, ['charging', 'journey']] += (end - start).seconds / 60
+            # else:
+            #     pred.loc[start_time_slot, ['charging', 'journey']] += (start.ceil(freq='30min') - start).seconds / 60
+            #     pred.loc[end_time_slot, ['charging', 'journey']] += (end - end_time_slot).seconds / 60
+
+            # calculate total energy consumption for the journey
+            journey_energy_consumption = sum(self.EV_data.loc[start:end]['P_total'])  # given in Joules
+
+            # -> this is where the SOC (charge level) consideration takes place (Boon)
+            if available_charge >= journey_energy_consumption:
+                # reduce the available charge of EV by the amount needed for this journey
+                available_charge = available_charge - journey_energy_consumption
+                continue
             else:
-                pred.loc[start_time_slot, ['charging', 'journey']] += (start.ceil(freq='30min') - start).seconds / 60
-                pred.loc[end_time_slot, ['charging', 'journey']] += (end - end_time_slot).seconds / 60
-
-            # calculate total charging time for the journey
-            journey_energy_consumption = sum(self.EV_data.loc[start:end]['P_total'])
-            charge_time = journey_energy_consumption / (charger_power * 60)
+                # reduce the additional charge needed to allow EV to complete this journey
+                journey_energy_consumption = journey_energy_consumption - available_charge
+                available_charge = 0
+            expected_charge = expected_charge + journey_energy_consumption  # keep track of expected charge level after charging
+            charge_time = journey_energy_consumption / (
+                        self.config_dict['Charger_power'] * 60)  # gives charging time in minutes
 
             # calculate number of time slots needed to charge EV
-            quotient = int(charge_time // 30) # full slots
+            quotient = int(charge_time // 30)  # full slots
             remainder = charge_time % 30
 
             # ignore any full slots and sort TOU slots by price
@@ -93,12 +178,50 @@ class charging_recommendation(object):
 
                 idx_offset += 1
 
-        # TOU threshold charging
-        pred.loc[pred['TOU'] <= threshold, 'charging'] = 30
+        # add SOC (charge level) consideration here (Boon)
+        # ASSUMPTION: charging rate is not dependent on current SOC. For more accurate result, implement a charging curve for the specific vehicle
+        charge_per_timeslot = self.config_dict['Charger_power'] * 60 * 30  # in J for each 30 min timeslot
+        # charge the EV if the additional charge does not cause charge level to exceed limit AND if the price of the timeslot is below threshold
+        while expected_charge < upper_limit:
+            # ignore any full slots and sort TOU slots by price
+            free_time_slots = pred.loc[pred['charging'] < 30].copy()
+            free_time_slots = free_time_slots.sort_values(by=['TOU'])
+            # if the cheapest TOU slot is below threshold, charge for that slot
+            if free_time_slots.iloc[0]['TOU'] <= self.config_dict['TOU_threshold']:
+                # if the cheapest slot is partially filled, then calculate how much charge the system would add for the rest of the timeslot
+                if free_time_slots.iloc[0]['charging'] != 0:
+                    remainder = free_time_slots.iloc[0]['charging']
+                    charge_time = 30 - remainder
+                    # if charging for the remainder of the slot will NOT exceed upper limit, allocate full slot
+                    if (upper_limit - expected_charge) >= (self.config_dict['Charger_power'] * 60 * charge_time):
+                        pred.loc[free_time_slots.iloc[[0]].index, 'charging'] = 30
+                        expected_charge += (self.config_dict['Charger_power'] * 60 * charge_time)
+                    # if charging for the remainder of the slot WILL exceed upper limit, charge up to upper limit
+                    else:
+                        charge_time = (upper_limit - expected_charge) / (self.config_dict['Charger_power'] * 60)
+                        pred.loc[free_time_slots.iloc[[0]].index, 'charging'] = charge_time + remainder
+                        expected_charge = upper_limit
+                else:
+                    # if charging for the full slot will NOT exceed upper limit, allocate full slot
+                    if (upper_limit - expected_charge) >= charge_per_timeslot:
+                        # fill up entire empty slot
+                        pred.loc[free_time_slots.iloc[[0]].index, 'charging'] = 30
+                        expected_charge += charge_per_timeslot
+                    # if charging for the full slot WILL exceed upper limit, allocate partial slot to charge up to upper limit
+                    else:
+                        charge_time = (upper_limit - expected_charge) / (self.config_dict['Charger_power'] * 60)
+                        pred.loc[free_time_slots.iloc[[0]].index, 'charging'] = charge_time
+                        expected_charge = upper_limit
+                # include the charge curve in the loop to update charge_per_timeslot according to current SOC
+            else:
+                print('TOU exceeds threshold, no additional charge slots allocated.')
+                break
+
+        # TOU threshold charging (old implementation without SOC consideration)
+        # pred.loc[pred['TOU'] <= threshold, 'charging'] = 30
 
         # subtract journey time from charging time
         pred['charging'] -= pred['journey']
         self.EV_data['charging'] = pred['charging']
 
         return pred.loc[pred['charging'] > 0, 'charging']
-
