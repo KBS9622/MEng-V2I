5ec77e8fc77603b1f88b8ab2cb8d03448ad36953
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2020-11-27 16:08:16 +0800

    test
    tested to see if sarima model prediction output would be affected by the length of prediction.
    
    Conclusion: No, length of prediction does not matter

--- src/main.py
+++ src/main.py
@@ -18,52 +18,53 @@ start_time = pd.to_datetime('2019-02-01 00:00:00')
 end_time = pd.to_datetime('2019-02-01 23:30:00')
 
 
-pred = TOU_obj.predict_and_compare(start_time1, end_time)
+# pred = TOU_obj.predict_and_compare(start_time1, end_time)
+hi = TOU_obj.predict_and_compare(start_time, end_time)
 # print(pred)
 
 ##########################################################################################
 
-file = '2012-03-01.csv'
-file2 = '2012-03-02.csv'
-subdir = '1119055_1'
+# file = '2012-03-01.csv'
+# file2 = '2012-03-02.csv'
+# subdir = '1119055_1'
 
-EV_obj = EV(file, subdir)
-EV_obj2 = EV(file2, subdir)
+# EV_obj = EV(file, subdir)
+# EV_obj2 = EV(file2, subdir)
 
-EV_obj.soc_over_time()
-EV_obj2.soc_over_time()
+# EV_obj.soc_over_time()
+# EV_obj2.soc_over_time()
 
-y = ['P_electric_motor', 'speed_mps', 'P_regen', 'n_rb', 'soc', 'P_total']
-file_name = ['energy_consumption.png', 'speed_profile.png', 'energy_consumption_with_regen.png',
-             'n_rb.png', 'soc.png', 'total_energy_conumption.png']
-EV_obj.graph_plotter(y=y, file_name=file_name, subdir=subdir, date=file.strip('.csv'))
-EV_obj2.graph_plotter(y=y, file_name=file_name, subdir=subdir, date=file2.strip('.csv'))
+# y = ['P_electric_motor', 'speed_mps', 'P_regen', 'n_rb', 'soc', 'P_total']
+# file_name = ['energy_consumption.png', 'speed_profile.png', 'energy_consumption_with_regen.png',
+#              'n_rb.png', 'soc.png', 'total_energy_conumption.png']
+# EV_obj.graph_plotter(y=y, file_name=file_name, subdir=subdir, date=file.strip('.csv'))
+# EV_obj2.graph_plotter(y=y, file_name=file_name, subdir=subdir, date=file2.strip('.csv'))
 
-cols_to_drop = ['cycle_sec', 'timestep', 'speed_mph', 'accel_meters_ps', 'speed_mps',
-                 'accel_mps2', 'P_wheels', 'P_electric_motor', 'n_rb', 'P_regen']
+# cols_to_drop = ['cycle_sec', 'timestep', 'speed_mph', 'accel_meters_ps', 'speed_mps',
+#                  'accel_mps2', 'P_wheels', 'P_electric_motor', 'n_rb', 'P_regen']
 
-P_total = EV_obj.data.copy()
-P_total = P_total.drop(columns=cols_to_drop)
-P_total = P_total.set_index('timestamp')
-P_total = P_total.set_index(P_total.index
-                            + DateOffset(days=(start_time.floor(freq='D')
-                                               - P_total.iloc[0].name.floor(freq='D')).days))
-# print('PTOTAL:\n{}'.format(P_total))
+# P_total = EV_obj.data.copy()
+# P_total = P_total.drop(columns=cols_to_drop)
+# P_total = P_total.set_index('timestamp')
+# P_total = P_total.set_index(P_total.index
+#                             + DateOffset(days=(start_time.floor(freq='D')
+#                                                - P_total.iloc[0].name.floor(freq='D')).days))
+# # print('PTOTAL:\n{}'.format(P_total))
 
-P_total2 = EV_obj2.data.copy()
-P_total2 = P_total2.drop(columns=cols_to_drop)
-P_total2 = P_total2.set_index('timestamp')
-P_total2 = P_total2.set_index(P_total2.index
-                            + DateOffset(days=(start_time1.floor(freq='D')
-                                               - P_total2.iloc[0].name.floor(freq='D')).days))
-# print('PTOTAL2:\n{}'.format(P_total2))
-##########################################################################################
+# P_total2 = EV_obj2.data.copy()
+# P_total2 = P_total2.drop(columns=cols_to_drop)
+# P_total2 = P_total2.set_index('timestamp')
+# P_total2 = P_total2.set_index(P_total2.index
+#                             + DateOffset(days=(start_time1.floor(freq='D')
+#                                                - P_total2.iloc[0].name.floor(freq='D')).days))
+# # print('PTOTAL2:\n{}'.format(P_total2))
+# ##########################################################################################
 
-#just runs the recommendation system that hasn't considered EV SOC
+# #just runs the recommendation system that hasn't considered EV SOC
 
-charging_recom_obj = charging_recommendation(P_total, pred, P_total2)
-print(charging_recom_obj.TOU_data.sort_values(by=['TOU']))
+# charging_recom_obj = charging_recommendation(P_total, pred, P_total2)
+# print(charging_recom_obj.TOU_data.sort_values(by=['TOU']))
 
-json_path = '/Users/koeboonshyang/Documents/GitHub/MEng-V2I/utils/user_config.json'
-charging_recom_obj.update_user_config(json_path)
-print(charging_recom_obj.recommend())
+# json_path = '/Users/koeboonshyang/Documents/GitHub/MEng-V2I/utils/user_config.json'
+# charging_recom_obj.update_user_config(json_path)
+# print(charging_recom_obj.recommend())
