cd0bb7d4e97e9e4239c4cf1edc4be5d233dfbd27
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-15 20:00:00 +0800

    update

--- src/train_TOU.py
+++ src/train_TOU.py
@@ -4,4 +4,4 @@ tou_file = 'full_data.csv'
 tou_subdir = 'data/TOU_Data'
 
 tou_obj = TOU(tou_file, tou_subdir)
-tou_obj.create_and_fit_model(seasonality = 52, fitted_model_filename='cluster_TOU.pickle')
+tou_obj.create_and_fit_model(seasonality = 12, fitted_model_filename='cluster_TOU.pickle')
