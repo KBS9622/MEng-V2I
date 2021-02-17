83c6343ac263b0d57ddb821d85e0a369ddd5240b
Rami <ramibilal@live.com>
2021-02-17 12:19:45 +0300

    updating .sh

--- src/train_TOU.py
+++ src/train_TOU.py
@@ -1,7 +1,9 @@
-from TOU_analysis_and_prediction import TOU
+# from TOU_analysis_and_prediction import TOU
+#
+# tou_file = 'full_data.csv'
+# tou_subdir = 'data/TOU_Data'
+#
+# tou_obj = TOU(tou_file, tou_subdir)
+# tou_obj.create_and_fit_model(seasonality = 12, fitted_model_filename='cluster_TOU.pickle')
 
-tou_file = 'full_data.csv'
-tou_subdir = 'data/TOU_Data'
-
-tou_obj = TOU(tou_file, tou_subdir)
-tou_obj.create_and_fit_model(seasonality = 12, fitted_model_filename='cluster_TOU.pickle')
+print("Hello World")
