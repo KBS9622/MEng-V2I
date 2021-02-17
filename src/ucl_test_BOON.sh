56a0db8f9c7619e7ac94b291b1ee18b1e130453f
Rami <ramibilal@live.com>
2021-02-17 13:44:26 +0300

    oopsies

--- src/ucl_test_BOON.sh
+++ src/ucl_test_BOON.sh
@@ -3,21 +3,21 @@
 # Batch script to run a serial job under SGE.
 
 # Request ten minutes of wallclock time (format hours:minutes:seconds).
-$ -l h_rt=0:10:0
+#$ -l h_rt=0:10:0
 
 # Request 100 gigabyte of RAM (must be an integer followed by M, G, or T)
-$ -l mem=100G
+#$ -l mem=100G
 
 # Request 15 gigabyte of TMPDIR space (default is 10 GB - remove if cluster is diskless)
-$ -l tmpfs=15G
+#$ -l tmpfs=15G
 
 # Set the name of the job.
-$ -N Serial_Job
+#$ -N Serial_Job
 
 # Set the working directory to somewhere in your scratch space.  
 #  This is a necessary step as compute nodes cannot write to $HOME.
 # Replace "<your_UCL_id>" with your UCL user ID.
-$ -wd /home/zceerch/Scratch
+#$ -wd /home/zceerch/Scratch
 
 # Your work should be done in $TMPDIR 
 cd $TMPDIR
