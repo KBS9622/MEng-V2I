3775ed9fa56d7942eab362185f3d15a7ced0e1e4
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-18 19:31:54 +0800

    push push

--- TimeSeriesPrediction/BOON_NN.sh
+++ TimeSeriesPrediction/BOON_NN.sh
@@ -33,9 +33,11 @@ cd ../MEng-V2I
 module unload compilers mpi
 module load compilers/gnu/4.9.2
 module load python3/recommended
-module load cuda/8.0.61-patch2/gnu-4.9.2
-module load cudnn/6.0/cuda-8.0
-module load tensorflow/1.4.1/gpu
+module load cuda/10.1.243/gnu-4.9.2
+module load cudnn/7.6.5.32/cuda-10.1
+module load tensorflow/1.14.0/gpu
+# install torch on my directory
+pip3 install --user torch==1.7.1+cu101 torchvision==0.8.2+cu101 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
 
 # Run the application and put the output into a file called SARIMA.txt, placed in the current directory, after any cd
 /usr/bin/time --verbose python3 TimeSeriesPrediction/main.py --network 'lstm' --transfer_learning True --mode 'train' > LSTM_old.txt
