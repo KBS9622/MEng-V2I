4249493b12cf3e36f5ab0820b66e501b7c11ac80
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-20 17:42:29 +0800

    Update train_dc.py

--- src/train_dc.py
+++ src/train_dc.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 dc_file = '12_sep_oct_nov_nov_dec.csv'
 
-dc_obj = DriveCycle(dc_file)
+dc_obj = DriveCycle(dc_file,preprocess_resample=True)
 # uncomment line below when training
-# dc_obj.create_and_fit_model(seasonality = 2, fitted_model_filename='fitted_model_dc.pickle')
+dc_obj.create_and_fit_model(seasonality = 2, fitted_model_filename='fitted_model_dc.pickle')
 
 # start_time1 = pd.to_datetime('2019-01-31 00:00:00')
 # end_time1 = pd.to_datetime('2019-01-31 23:30:00')
-start_time = pd.to_datetime('2017-12-29 20:10:59')
-end_time = pd.to_datetime('2017-12-29 20:32:00')
+start_time = pd.to_datetime('2017-12-29 00:00:00')
+end_time = pd.to_datetime('2017-12-29 23:59:59')
 
 
 pred = dc_obj.predict_and_compare(start_time, end_time, fitted_model_filename= 'fitted_model_dc.pickle')
