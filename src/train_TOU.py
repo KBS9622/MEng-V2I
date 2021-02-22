7662869a33414f443c2e760cf4fa7db1e37268b2
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-22 18:45:45 +0800

    Update train_TOU.py

--- src/train_TOU.py
+++ src/train_TOU.py
@@ -7,14 +7,14 @@ tou_subdir = "/data/TOU_Data"
 
 tou_obj = TOU(tou_training_file, tou_subdir)
 # uncomment line below when training
-tou_obj.create_and_fit_model(seasonality=48, fitted_model_filename='cluster_TOU.pickle')
+tou_obj.create_and_fit_model(seasonality=48, fitted_model_filename='cluster_TOU_48.pickle')
 
 # start_time1 = pd.to_datetime('2019-01-31 00:00:00')
 # end_time1 = pd.to_datetime('2019-01-31 23:30:00')
 start_time = pd.to_datetime('2021-01-16 00:00:00')
 end_time = pd.to_datetime('2021-01-16 23:30:00')
 
-pred = tou_obj.predict_and_compare(start_time, end_time, fitted_model_filename='fitted_model.pickle')
+pred = tou_obj.predict_and_compare(start_time, end_time, fitted_model_filename='cluster_TOU_48.pickle')
 
 # with open('test_file.txt', 'w') as f:
 #     f.write("This is a sample file")
