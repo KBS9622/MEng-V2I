d11107de162a44df0dcc96828397a84770668cbb
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-24 17:27:43 +0800

    Update DC_gen_tool.py

--- DC_generator/DC_gen_tool.py
+++ DC_generator/DC_gen_tool.py
@@ -1,11 +1,37 @@
 
 
-
+class DP(object):
+    def __init__(self):
+        velocity_noise = None
+        accel = None
+        cruise = None
+        decel = None
+        idle = None
+        t_DP = None
 
 
 
 
 class DCGT(object):
-    def DC(self):
+
+    def __init__(self):
+        pass
+
+    def DC(self, dc_length):
         """ Module 1: Drive Cycle
         """
+        t_DC = dc_length
+        sum_t_DS = 0
+        while t_DC > sum_t_DS:
+            pass
+        pass
+
+    def DS(self):
+        """ Module 2: Drive Scenario
+        """
+        pass
+
+    def DP(self):
+        """ Module 3: Drive Pulse
+        """
+        pass
