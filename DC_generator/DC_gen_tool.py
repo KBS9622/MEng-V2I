01a01a5098e4c62eb4bd316d5257bf09c7b77def
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-24 19:41:44 +0800

    .

--- DC_generator/DC_gen_tool.py
+++ DC_generator/DC_gen_tool.py
@@ -1,6 +1,8 @@
-
+import math
 
 class DP(object):
+    """ Module 3: Drive Pulse
+    """
     def __init__(self):
         velocity_noise = None
         accel = None
@@ -9,29 +11,30 @@ class DP(object):
         idle = None
         t_DP = None
 
-
-
-
-class DCGT(object):
-
-    def __init__(self):
+    def load_csv_data(self):
         pass
 
-    def DC(self, dc_length):
-        """ Module 1: Drive Cycle
-        """
+    def velocity_noise(self):
+        #EQN 5
+        y_FS_t = 
+        pass
+
+class DS(object):
+    """ Module 2: Drive Scenario
+    """
+    def __init__(self):
+        
+
+class DC(object):
+    """ Module 1: Drive Cycle
+    """
+    def __init__(self, dc_length):
+        
         t_DC = dc_length
         sum_t_DS = 0
         while t_DC > sum_t_DS:
             pass
         pass
 
-    def DS(self):
-        """ Module 2: Drive Scenario
-        """
-        pass
 
-    def DP(self):
-        """ Module 3: Drive Pulse
-        """
-        pass
+
