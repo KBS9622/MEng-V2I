55ec98de9a7a0c842fdd93ee5bbfacaf469a40c0
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-02-25 19:52:31 +0800

    gogogo

--- DC_generator/DC_gen_tool.py
+++ DC_generator/DC_gen_tool.py
@@ -1,40 +1,68 @@
+import os
 import math
+from sympy import *
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit, least_squares
+from lmfit import minimize, Minimizer, Parameters, Parameter, report_fit
+import matplotlib.pyplot as plt
+import lmfit
 
-class DP(object):
-    """ Module 3: Drive Pulse
+
+def load_csv_data(file_name, subdir=''):
     """
-    def __init__(self):
-        velocity_noise = None
-        accel = None
-        cruise = None
-        decel = None
-        idle = None
-        t_DP = None
+    Loads data from .csv file in to DataFrame
 
-    def load_csv_data(self):
-        pass
-
-    def velocity_noise(self):
-        #EQN 5
-        y_FS_t = 
-        pass
-
-class DS(object):
-    """ Module 2: Drive Scenario
+    :param file_name: .csv file name in string
+    :param subdir: optional parameter to specify the subdirectory of the file
+    :return: extracted data in DataFrame
     """
-    def __init__(self):
-        
 
-class DC(object):
-    """ Module 1: Drive Cycle
-    """
-    def __init__(self, dc_length):
-        
-        t_DC = dc_length
-        sum_t_DS = 0
-        while t_DC > sum_t_DS:
-            pass
-        pass
+    file_dir = os.path.realpath('../')
+    print(file_dir)
+    for root, dirs, files in os.walk(file_dir):
+        if root.endswith(subdir):
+            for name in files:
+                if name == file_name:
+                    file_path = os.path.join(root, name)
+
+    df = pd.read_csv(file_path)
+
+    return df
+
+if __name__ == '__main__':
+    LMparams = Parameters()
+    LMparams.add('A_FS', value = 1.)
+    LMparams.add('w_FS', value = 1., min = 0, max = 0.01*2*math.pi)
+    LMparams.add('phi_FS', value = 1., min = -math.pi, max = math.pi)
+
+    
+
+    # print(os.getcwd())
+    # loads the csv file
+    subdir = 'caltrans_processed_drive_cycles/data/1035198_1'
+    file_name = '2012-05-22.csv'
+    data = load_csv_data(file_name, subdir)
+    # get a slice of the data with a relatively long cruising period
+    data = data.iloc[1002:1096,:]
+    data.reindex()
+    # print(data)
+    plt.plot(data.loc[:,'timestamp'], data.loc[:,'speed_mph'])
+    # plt.show()
+    # get the slice of ONLY cruising period
+    cruising_data = data.iloc[25:86,:]
 
 
+    x = np.linspace(1,len(cruising_data),len(cruising_data))
+    temp = cruising_data.loc[:,'speed_mph'].to_numpy()
+    print(x)
+    y = temp- temp.mean()
+    print(type(y))
+    plt.plot(x, y, 'b')
+    plt.show()
 
+# x = np.linspace(1, 10, 250)
+# np.random.seed(0)
+# y = 3.0 * np.exp(-x / 2) - 5.0 * np.exp(-(x - 0.1) / 10.) + 0.1 * np.random.randn(x.size)
+# plt.plot(x, y, 'b')
+# plt.show()
