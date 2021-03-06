ae25fd14bc73713819de13630960726cbcfb8255
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-03-06 17:19:17 +0800

    what

--- playground.py
+++ playground.py
@@ -21,13 +21,17 @@ import pandas as pd
 # XY_select = selection(XY, limitXY=[[-2,+2],[-2,+2]])
 
 
-file_path = 'Device12_formatted.csv'
+file_path = 'Device13_formatted.csv'
 df = pd.read_csv(file_path)
 # df = df.set_index('timestamp')
 # start_time = pd.to_datetime('2017-09-25 13:41:41')
 # end_time = pd.to_datetime('2017-09-30 23:30:00')
 # df = df.loc[0:100000,:]
-df = df.loc[200001:300000,:]
+df = df.loc[100001:200000,:]
+# df = df.loc[200001:300000,:]
+print(df['speed_mps'].min())
+print(df['speed_mps'].max())
+print(df['accel_mps2'].min())
 print(df['accel_mps2'].max())
 x = df['speed_mps'].to_numpy()
 y = df['accel_mps2'].to_numpy()
@@ -40,8 +44,12 @@ fig = plt.figure() #create a canvas, tell matplotlib it's 3d
 ax = fig.add_subplot(111, projection='3d')
 
 
-hist, xedges, yedges = np.histogram2d(x, y, bins=(10,10), range = [[0,+35],[-6,+14]]) # you can change your bins, and the range on which to take data
+hist, xedges, yedges = np.histogram2d(x, y, bins=10, range = [[0,+10],[-6,+14]]) # you can change your bins, and the range on which to take data
 # hist is a 7X7 matrix, with the populations for each of the subspace parts.
+print(hist)
+print(xedges)
+print(yedges)
+
 xpos, ypos = np.meshgrid(xedges[:-1]+xedges[1:], yedges[:-1]+yedges[1:]) -(xedges[1]-xedges[0])
 
 
@@ -60,13 +68,45 @@ min_height = np.min(dz)
 rgba = [cmap((k-min_height)/max_height) for k in dz] 
 
 ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
-plt.title("speed vs accel for ID 12 200k - 300k Data")
+plt.title("speed vs accel for ID 13 100k - 200k Data")
 plt.xlabel("speed (mps)")
 plt.ylabel("accel (mps2)")
-plt.savefig("Device12_200k_to_300k_observations")
+plt.savefig("Zoomed_id13_100k_to_200k_observations")
 plt.show()
 
+# fig = plt.figure()          #create a canvas, tell matplotlib it's 3d
+# ax = fig.add_subplot(111, projection='3d')
 
+# #make histogram stuff - set bins - I choose 20x20 because I have a lot of data
+# hist, xedges, yedges = np.histogram2d(x, y, bins=(4,4), range = [[10,20],[-6,1]])
+# print(hist)
+# print(xedges)
+# print(yedges)
+# # xpos, ypos = np.meshgrid(xedges[:-1]+xedges[1:], yedges[:-1]+yedges[1:])
+# xpos, ypos = np.meshgrid(xedges[:-1]+xedges[1:], yedges[:-1]+yedges[1:]) -(xedges[1]-xedges[0])
+# print(xpos)
+# print(ypos)
+
+# xpos = xpos.flatten()/2.
+# ypos = ypos.flatten()/2.
+# zpos = np.zeros_like (xpos)
+
+# dx = xedges [1] - xedges [0]
+# dy = yedges [1] - yedges [0]
+# dz = hist.flatten()
+
+# cmap = plt.cm.get_cmap('jet') # Get desired colormap - you can change this!
+# max_height = np.max(dz)   # get range of colorbars so we can normalize
+# min_height = np.min(dz)
+# # scale each z to [0,1], and get their rgb values
+# rgba = [cmap((k-min_height)/max_height) for k in dz] 
+
+# ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
+# plt.title("X vs. Y Amplitudes for ____ Data")
+# plt.xlabel("My X data source")
+# plt.ylabel("My Y data source")
+# plt.savefig("Your_title_goes_here")
+# plt.show()
 
 
 
