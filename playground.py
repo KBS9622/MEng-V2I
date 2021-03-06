612bbbd0e0b55e34b1d2cd0d5650c3dd9e30c99c
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2021-03-06 18:22:20 +0800

    yay

--- playground.py
+++ playground.py
@@ -23,6 +23,7 @@ import pandas as pd
 
 file_path = 'Device13_formatted.csv'
 df = pd.read_csv(file_path)
+df = df[(df['speed_mps']!=0) & (df['accel_mps2']!=0)]
 # df = df.set_index('timestamp')
 # start_time = pd.to_datetime('2017-09-25 13:41:41')
 # end_time = pd.to_datetime('2017-09-30 23:30:00')
@@ -36,25 +37,21 @@ print(df['accel_mps2'].max())
 x = df['speed_mps'].to_numpy()
 y = df['accel_mps2'].to_numpy()
 
-# xAmplitudes = np.array(XY_select)[:,0]#your data here
-# yAmplitudes = np.array(XY_select)[:,1]#your other data here
-
-
 fig = plt.figure() #create a canvas, tell matplotlib it's 3d
 ax = fig.add_subplot(111, projection='3d')
 
 
-hist, xedges, yedges = np.histogram2d(x, y, bins=10, range = [[0,+10],[-6,+14]]) # you can change your bins, and the range on which to take data
+hist, xedges, yedges = np.histogram2d(x, y, bins=10)#, range = [[0,+30],[-0.5,0.5]]) # you can change your bins, and the range on which to take data
 # hist is a 7X7 matrix, with the populations for each of the subspace parts.
 print(hist)
 print(xedges)
 print(yedges)
 
-xpos, ypos = np.meshgrid(xedges[:-1]+xedges[1:], yedges[:-1]+yedges[1:]) -(xedges[1]-xedges[0])
+xpos, ypos = np.meshgrid(xedges[:-1], yedges[:-1], indexing="ij")
 
 
-xpos = xpos.flatten()*1./2
-ypos = ypos.flatten()*1./2
+xpos = xpos.flatten()
+ypos = ypos.flatten()
 zpos = np.zeros_like (xpos)
 
 dx = xedges [1] - xedges [0]
@@ -71,42 +68,36 @@ ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
 plt.title("speed vs accel for ID 13 100k - 200k Data")
 plt.xlabel("speed (mps)")
 plt.ylabel("accel (mps2)")
-plt.savefig("Zoomed_id13_100k_to_200k_observations")
+# plt.savefig("Zoomed_id13_100k_to_200k_observations")
 plt.show()
 
-# fig = plt.figure()          #create a canvas, tell matplotlib it's 3d
-# ax = fig.add_subplot(111, projection='3d')
 
-# #make histogram stuff - set bins - I choose 20x20 because I have a lot of data
-# hist, xedges, yedges = np.histogram2d(x, y, bins=(4,4), range = [[10,20],[-6,1]])
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# hist, xedges, yedges = np.histogram2d(x, y, bins=4)
 # print(hist)
 # print(xedges)
 # print(yedges)
-# # xpos, ypos = np.meshgrid(xedges[:-1]+xedges[1:], yedges[:-1]+yedges[1:])
-# xpos, ypos = np.meshgrid(xedges[:-1]+xedges[1:], yedges[:-1]+yedges[1:]) -(xedges[1]-xedges[0])
+
+# # Construct arrays for the anchor positions of the 16 bars.
+# xpos, ypos = np.meshgrid(xedges[:-1], yedges[:-1], indexing="ij")
 # print(xpos)
 # print(ypos)
+# xpos = xpos.ravel()
+# ypos = ypos.ravel()
+# zpos = 0
 
-# xpos = xpos.flatten()/2.
-# ypos = ypos.flatten()/2.
-# zpos = np.zeros_like (xpos)
+# # Construct arrays with the dimensions for the 16 bars.
+# dx = dy = 0.5 * np.ones_like(zpos)
+# dz = hist.ravel()
 
-# dx = xedges [1] - xedges [0]
-# dy = yedges [1] - yedges [0]
-# dz = hist.flatten()
+# ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average')
 
-# cmap = plt.cm.get_cmap('jet') # Get desired colormap - you can change this!
-# max_height = np.max(dz)   # get range of colorbars so we can normalize
-# min_height = np.min(dz)
-# # scale each z to [0,1], and get their rgb values
-# rgba = [cmap((k-min_height)/max_height) for k in dz] 
-
-# ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color=rgba, zsort='average')
-# plt.title("X vs. Y Amplitudes for ____ Data")
-# plt.xlabel("My X data source")
-# plt.ylabel("My Y data source")
-# plt.savefig("Your_title_goes_here")
 # plt.show()
+
+
+
+
 
 
 
